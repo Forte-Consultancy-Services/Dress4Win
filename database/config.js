225e30bf91ea5741e67970557dd59de63d1e5625
Linda Lim <CookiesNCream@users.noreply.github.com>
2021-04-03 22:31:00 +0800

    Sixth Commit

--- database/config.js
+++ database/config.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
   host     : '',
   port     : '3306',
   user     : 'root',
-  password : '',
+  password : 'P@ssw0rd2020',
   database : 'eCommerce'
 });
 
